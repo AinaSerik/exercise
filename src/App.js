036af04afa7896ed1375f880756e59dd6b36c309
Ainagul Borambayeva <ainagulkzt@gmail.com>
2022-06-12 12:32:17 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import '/App.css';
+import './App.css';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box width='400px'>
+    <Box width='400px' sx={{ width: { xl: '1488px' }}} m='auto'>
       <Navbar />
       <Routes>
         <Routes path='/' element={<Home/>}/>
