2802072cb6ab51da3304f6f44cebbf6eb7ec9df6
Ainagul Borambayeva <ainagulkzt@gmail.com>
2022-06-15 07:02:09 -0400

    exercise detail added

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -9,6 +9,35 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   
   const [currentPage, setCurrentPage] = useState(1);
   const exercisesPerPage = 9;
+
+    const indexOfLastExercise = currentPage * exercisesPerPage;
+    const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+    const currentExercises = exercises.slice (indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrolling({ top: 1800, behavior: 'smooth'})
+  }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if(bodyPart === 'all') {
+        exercisesData = await fetchData
+          ('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData
+          (`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    }
+
+    fetchExercisesData();
+  }, [bodyPart]);
+  
   return (
     <Box id='exercises'
     sx={{mt: { lg: '110px' }}}
@@ -22,7 +51,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       lg: '110px', xs: '50px'
     }}}
     flexWrap='wrap' justifyContent='center'>
-    {exercises.map((exercise, index) => (
+    {currentExercises.map((exercise, index) => (
     <ExerciseCard key={index} exercise={exercise}/>
     ))}
     </Stack>
